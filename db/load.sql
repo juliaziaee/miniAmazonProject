2adbfa7fc718168897cfe2897d38394d40c40e04
nicolemalpeli <nicole.malpeli@duke.edu>
2021-11-10 18:35:52 -0500

    generate product id

--- db/load.sql
+++ db/load.sql
@@ -4,6 +4,7 @@
 SELECT pg_catalog.setval('public.users_id_seq', 1000, false);
 \COPY Seller FROM 'data/SellerGenerated.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Products FROM 'data/ProductsGenerated.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.products_id_seq', 200, false);
 \COPY Funding FROM 'data/FundingGenerated.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Purchases FROM 'data/PurchasesGenerated.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Cart FROM 'data/CartGenerated.csv' WITH DELIMITER ',' NULL '' CSV
