f85f1324b736dec896d32ad14495306916b5c29e
Clara Lyra <ccl37@duke.edu>
2021-12-05 17:26:16 -0500

    modified UI for up and down vote buttons

--- app/models/reviews.py
+++ app/models/reviews.py
@@ -2,14 +2,13 @@ from flask import current_app as app
 
 
 class ProdReviews:
-    def __init__(self, firstname, lastname, uid, pid, rating, numUpVotes, numDownVotes, review, DateTime):
+    def __init__(self, firstname, lastname, uid, pid, rating, numVotes, review, DateTime):
         self.firstname = firstname
         self.lastname = lastname
         self.uid = uid
         self.pid = pid
         self.rating = rating
-        self.numUpVotes = numUpVotes
-        self.numDownVotes = numDownVotes
+        self.numVotes = numVotes
         self.review = review
         self.DateTime = DateTime
 
@@ -17,7 +16,7 @@ class ProdReviews:
     def get_all(pid):
        
         rows = app.db.execute('''
-SELECT firstname, lastname, uid, pid, rating, numUpVotes, numDownVotes, review, DateTime
+SELECT firstname, lastname, uid, pid, rating, numVotes, review, DateTime
 FROM ProductReview, Users
 WHERE ProductReview.uid = Users.id AND ProductReview.pid = :pid
 ORDER BY DateTime DESC
